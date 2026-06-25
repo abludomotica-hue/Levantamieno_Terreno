730443b12ceac5a5d1da18cbd23c9affbc9e42d8
Ablutech Developer <developer@ablutech.com>
2026-06-25 02:55:30 -0400

    fix: Optimize middleware bundle size for Vercel Edge

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,9 @@
-export { auth as middleware } from './auth'
+import NextAuth from 'next-auth'
+import { authConfig } from './auth.config'
+
+export const { auth: middleware } = NextAuth(authConfig)
 
 export const config = {
   matcher: ['/((?!api|_next/static|_next/image|favicon.ico|login).*)'],
 }
+
